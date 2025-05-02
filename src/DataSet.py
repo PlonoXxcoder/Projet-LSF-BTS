3ebd14d8e72295367b1057e3285aa6a0270215c0
PlonoXxcoder <92154741+PlonoXxcoder@users.noreply.github.com>
2025-05-02 16:14:20 +0200

    Update

--- src/DataSet.py
+++
@@ -1,4 +0,0 @@
-from lsfb_dataset import Downloader # CORRECT (pour l'ancienne version)
-
-downloader = Downloader(dataset='cont', destination="./destination/folder", include_videos=True)
-downloader.download()
